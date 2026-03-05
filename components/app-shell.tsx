f689b4c239eaef6b3482c4a9f59e5fd067391760
Agustin MIOTTI <amiotti@secco.local>
2026-03-05 08:00:13 -0300

    minor changes

--- components/app-shell.tsx
+++ components/app-shell.tsx
@@ -97,7 +97,7 @@ export async function AppShell({ children }: { children: ReactNode }) {
 
       <footer className="site-footer">
         <div className="container site-footer-inner">
-          <p>(c) {new Date().getFullYear()}  {"PRODE Mundial 2026. Sitio web desarrollado por "} <a href="https://github.com/amiotti/ProdeAmigos" target="_blank" rel="noreferrer">
+          <p>(c) {new Date().getFullYear()}  PRODE Mundial 2026. Sitio web desarrollado por  <a href="https://github.com/amiotti/ProdeAmigos" target="_blank" rel="noreferrer">
             Agustin Miotti
           </a>.</p>
 
